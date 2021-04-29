b4b3e536e515519a813f757561c6f9b2f379870b
Robert Dupplaw <dupplawrobert@gmail.com>
2021-04-29 11:34:13 +0000

    removed superfluous unit tests

--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -1,21 +1,6 @@
 require 'secret_diary'
 
 describe SecretDiary do
-  context 'instance' do
-    it 'responds to #lock' do
-      expect(SecretDiary.new()).to respond_to(:lock)
-    end
-    it 'responds to #unlock' do
-      expect(SecretDiary.new()).to respond_to(:unlock)
-    end
-    it 'responds to #add_entry' do
-      expect(SecretDiary.new()).to respond_to(:add_entry)
-    end
-    it 'responds to #get_entries' do
-      expect(SecretDiary.new()).to respond_to(:get_entries)
-    end
-  end
-
   describe '#lock' do
     let (:diary) {SecretDiary.new}
 
